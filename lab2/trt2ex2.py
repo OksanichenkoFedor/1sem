adad7012a653812a93f454b2206eab8008736e48
OksanichenkoFedor <71315422+OksanichenkoFedor@users.noreply.github.com>
2020-09-22 00:10:22 +0300

    Add files via upload

---
+++ lab2/trt2ex2.py
@@ -0,0 +1,96 @@
+import turtle as trt
+import numpy as np
+from random import *
+N = 50
+def write1num (i):
+    if i==0:
+        wrzero()
+    elif i==4:
+        wrfour()
+    elif i==1:
+        wrone()
+    elif i==7:
+        wrseven()
+
+        
+def wrzero ():
+    trt.forward(N)
+    trt.left(90)
+    trt.forward(2*N)
+    trt.left(90)
+    trt.forward(N)
+    trt.left(90)
+    trt.forward(2*N)
+    trt.left(90)
+    trt.penup()
+    trt.forward(1.5*N)
+    trt.pendown()
+def wrone ():
+    trt.penup()
+    trt.forward(N)
+    trt.pendown()
+    trt.left(90)
+    trt.forward(2*N)
+    trt.left(135)
+    trt.forward(1.4*N)
+    trt.penup()
+    trt.right(180)
+    trt.forward(1.4*N)
+    trt.right(135)
+    trt.forward(2*N)
+    trt.left(90)
+    trt.forward(N/2)
+    trt.pendown()
+def wrfour ():
+    trt.penup()
+    trt.forward(N)
+    trt.pendown()
+    trt.left(90)
+    trt.forward(2*N)
+    trt.left(180)
+    trt.forward(N)
+    trt.right(90)
+    trt.forward(N)
+    trt.right(90)
+    trt.forward(N)
+    trt.penup()
+    trt.right(180)
+    trt.forward(N)
+    trt.left(90)
+    trt.forward(N)
+    trt.right(90)
+    trt.forward(N)
+    trt.left(90)
+    trt.forward(N/2)
+    trt.pendown()
+def wrseven ():
+    trt.left(90)
+    trt.forward(N)
+    trt.penup()
+    trt.forward(N)
+    trt.pendown()
+    trt.right(90)
+    trt.forward(N)
+    trt.right(135)
+    trt.forward(np.sqrt(2)*N)
+    trt.left(180)
+    trt.penup()
+    trt.forward(np.sqrt(2)*N)
+    trt.right(135)
+    trt.forward(2*N)
+    trt.left(90)
+    trt.forward(N/2)
+    trt.pendown()
+def writenum (s):
+    A = list(map(int, s))
+    trt.penup()
+    trt.left(180)
+    trt.forward(1.5*N*(len(A)/2))
+    trt.right(180)
+    trt.pendown()
+    for i in range(len(A)):
+        write1num(int(A[i]))
+      
+trt.shape('turtle')
+writenum("141700")
+#writenum("141700")
